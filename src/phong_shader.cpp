da11a6708a4887ba5a49ca0894a46caba3365865
Elijah Nicasio <enica001@hammer.cs.ucr.edu>
2018-01-21 13:35:19 -0800

    Added specular for Phong Shading

--- src/phong_shader.cpp
+++ src/phong_shader.cpp
@@ -13,15 +13,31 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     // For Ambient
     
 
-    // For Diffuse
+    // For loop for each light
     for(size_t i = 0; i < world.lights.size(); i++)
     {
         double distance = (intersection_point - world.lights[i]->position).magnitude();
         double divisor = distance * distance;
+        // illum is light intensity
         vec3 illum = world.lights[i]->Emitted_Light(ray) / (divisor);
+ 
+        // lightVector is vector from point to light
+        vec3 lightVector = (world.lights[i]->position - intersection_point).normalized();
+
+        // For Diffuse
         color += illum  * color_diffuse 
-                 * std::max(0.0, dot(same_side_normal,
-                                     (world.lights[i]->position - intersection_point).normalized()));
+                 * std::max(0.0, dot(same_side_normal, lightVector));
+
+        // For Specular
+        
+        // ref is reflection of lightVector
+        vec3 ref = -1.0 * lightVector + 2.0 * (dot(lightVector, same_side_normal)) * same_side_normal;
+        // viewer is vector to viewer, or the camera in this case
+        vec3 viewer = (world.camera.position - intersection_point).normalized();
+
+        color += illum * color_specular
+                 * pow(std::max(0.0, dot(ref, viewer)), specular_power);
     }
+
     return color;
 }
